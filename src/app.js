054fd211cc089711e01bfaa538608b25b3be9273
Vano Devium <webdevium@gmail.com>
2019-10-15 15:45:17 +0300

    initial

---
+++ src/app.js
@@ -0,0 +1,39 @@
+const {clipboard} = require('electron');
+
+const Typograf = require('typograf');
+const Eyo = require('eyo-kernel');
+
+const safeEyo = new Eyo();
+safeEyo.dictionary.loadSafeSync();
+
+const typograf = new Typograf({locale: ['ru', 'en-US'], live: true});
+const $editor = document.querySelector('#editor');
+const $result = document.querySelector('#result');
+
+$editor.focus();
+
+let lastValue = $editor.value;
+
+$editor.addEventListener(
+  'input',
+  () => {
+    const value = $editor.value;
+    if (value !== lastValue) {
+      const pos = getCaretPosition($editor);
+      lastValue = typograf.execute(value);
+      setCaretPosition($editor, pos === value.length ? lastValue.length : pos);
+    }
+
+    $result.value = typograf.execute(safeEyo.restore(lastValue));
+    clipboard.writeText($result.value);
+  },
+  false
+);
+
+function getCaretPosition(element) {
+  return element.selectionStart || 0;
+}
+
+function setCaretPosition(element, pos) {
+  return typeof element.setSelectionRange === 'function' && element.setSelectionRange(pos, pos);
+}
---
+++ src/index.js
@@ -0,0 +1,104 @@
+const {app, BrowserWindow, Menu} = require('electron');
+const isMac = () => process.platform === 'darwin';
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
+
+let mainWindow;
+let splashWindow;
+
+const createMainWindow = () => {
+  mainWindow = new BrowserWindow({
+    icon: './resources/icon.png',
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    },
+    show: false
+  });
+
+  const menu = Menu.buildFromTemplate([
+    {
+      label: '&Menu',
+      submenu: [
+        {
+          label: 'Exit',
+          click() {
+            app.quit();
+          },
+          accelerator: 'CmdOrCtrl+Q'
+        }
+      ]
+    }
+  ]);
+  Menu.setApplicationMenu(menu);
+
+  // And load the index.html of the app.
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools();
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+
+  mainWindow.once('show', () => {
+    splashWindow.destroy();
+  });
+
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show();
+  });
+};
+
+const createSplashWindow = () => {
+  splashWindow = new BrowserWindow({
+    width: 512,
+    height: 512,
+    transparent: true,
+    frame: false,
+    alwaysOnTop: true
+  });
+
+  splashWindow.loadURL(`file://${__dirname}/splash.html`);
+
+  splashWindow.on('closed', () => {
+    splashWindow = null;
+  });
+};
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', () => {
+  createSplashWindow();
+  createMainWindow();
+});
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (!isMac()) {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createMainWindow();
+  }
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.
